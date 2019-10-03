ece264b92b2b97789f1193a4416061e47c2df557
dstanfield <dstanfieldva@gmail.com>
2019-10-03 11:24:43 -0400

    deletos supreme

--- addCert.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-SET JRE_HOME="C:\Program Files (x86)\Common Files\MicroStrategy\JRE\180_77\Win64"
-%JRE_HOME%\bin\keytool -import -alias mstrca -keystore %JRE_HOME%\lib\security\cacerts -storepass changeit -noprompt -file mstrca.cer
--- createKeytabWin.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-SET JRE_HOME="C:\Program Files (x86)\Common Files\MicroStrategy\JRE\180_77\Win64"
-
-%JRE_HOME%\bin\ktab -a cesar-268949Web@LABS.MICROSTRATEGY.COM m$tr!23 -k "C:\kerbutil\krb5.keytab"
-%JRE_HOME%\bin\ktab -a HTTP/cesar-268949.labs.microstrategy.com@LABS.MICROSTRATEGY.COM m$tr!23 -k "C:\kerbutil\krb5.keytab"
-%JRE_HOME%\bin\ktab -a HTTP/cesar-268949@LABS.MICROSTRATEGY.COM m$tr!23 -k "C:\kerbutil\krb5.keytab"
